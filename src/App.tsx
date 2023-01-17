31b82f45f76b6b141751d927b2bb196a3a0fa08e
Emre <emredemirel39@gmail.com>
2023-01-17 19:20:05 +0300

    Added soon section

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import FullScreenSection from "./components/FullScreenSection";
 import Header from "./components/Header";
 import ScrollSign from "./components/ScrollSign";
 import SkillsSlider from "./components/SkillsSlider";
+import SoonSection from "./components/SoonSection";
 
 function App() {
   const [mousePosition, setMousePosition] = useState({ left: 0, top: 0 });
   const parallaxRef = useRef<HTMLDivElement>(null);
+  const onDevelopment: boolean = true;
 
   const handleMouseMove = (
     e: React.MouseEvent<HTMLDivElement | HTMLElement, MouseEvent>
@@ -35,46 +37,52 @@ function App() {
       className="App overflow-y-scroll h-screen font-serif text-gray-200 w-full snap-start bg-auto bg-repeat"
       onMouseMove={(e) => handleMouseMove(e)}
     >
-      <Header navLinks={navLinks} handleMouseMove={handleMouseMove} />
-      <FullScreenSection id="1">
-        <h1 className="mb-6 lg:mb-12 text-4xl md:text-6xl lg:text-7xl text-center">
-          {" "}
-          Hello, It's Emre!
-        </h1>
-        <p className="text-center text-[#a0a3a8]">
-          I am a passionate and dedicated frontend developer creating visually
-          stunning and responsive websites and web apps.
-          {mousePosition.left} {mousePosition.top}
-        </p>
-        <ScrollSign direction="down" />
-      </FullScreenSection>
-      <FullScreenSection id="2">
-        <h4 className="mb-12 text-6xl sm:text-7xl text-center border-b-2 border-blue-500 border-xl">
-          About Me
-        </h4>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
-          iure porro fugiat distinctio deserunt ea! Doloribus dolorem voluptates
-          saepe fugiat aliquid?
-        </p>
-      </FullScreenSection>
-      <FullScreenSection id="3">
-        <h4 className="mb-12 text-6xl sm:text-7xl text-center">Skills</h4>
-        <SkillsSlider height={320} sliderContent={sliderContent} />
-      </FullScreenSection>
-      <FullScreenSection id="4">
-        <h4 className="mb-12 text-6xl sm:text-7xl text-center border-b-2 border-blue-500 border-xl">
-          Projects
-        </h4>
-      </FullScreenSection>
-      <FullScreenSection id="5">
-        <h4 className="mb-12 text-6xl sm:text-7xl text-center">Contact</h4>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
-          iure porro fugiat distinctio deserunt ea! Doloribus dolorem voluptates
-          saepe fugiat aliquid?
-        </p>
-      </FullScreenSection>
+      {onDevelopment ? (
+        <SoonSection />
+      ) : (
+        <>
+          <Header navLinks={navLinks} handleMouseMove={handleMouseMove} />
+          <FullScreenSection id="1">
+            <h1 className="mb-6 lg:mb-12 text-4xl md:text-6xl lg:text-7xl text-center">
+              {" "}
+              Hello, It's Emre!
+            </h1>
+            <p className="text-center text-[#a0a3a8]">
+              I am a passionate and dedicated frontend developer creating
+              visually stunning and responsive websites and web apps.
+              {mousePosition.left} {mousePosition.top}
+            </p>
+            <ScrollSign direction="down" />
+          </FullScreenSection>
+          <FullScreenSection id="2">
+            <h4 className="mb-12 text-6xl sm:text-7xl text-center border-b-2 border-blue-500 border-xl">
+              About Me
+            </h4>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Veritatis iure porro fugiat distinctio deserunt ea! Doloribus
+              dolorem voluptates saepe fugiat aliquid?
+            </p>
+          </FullScreenSection>
+          <FullScreenSection id="3">
+            <h4 className="mb-12 text-6xl sm:text-7xl text-center">Skills</h4>
+            <SkillsSlider height={320} sliderContent={sliderContent} />
+          </FullScreenSection>
+          <FullScreenSection id="4">
+            <h4 className="mb-12 text-6xl sm:text-7xl text-center border-b-2 border-blue-500 border-xl">
+              Projects
+            </h4>
+          </FullScreenSection>
+          <FullScreenSection id="5">
+            <h4 className="mb-12 text-6xl sm:text-7xl text-center">Contact</h4>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Veritatis iure porro fugiat distinctio deserunt ea! Doloribus
+              dolorem voluptates saepe fugiat aliquid?
+            </p>
+          </FullScreenSection>
+        </>
+      )}
     </div>
   );
 }
